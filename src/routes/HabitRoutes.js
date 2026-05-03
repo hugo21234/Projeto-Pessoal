7931b5926d8cece2d9824a4587279b849d2ddd05
hugo21234 <hugo.deoliveira.legacy@gmail.com>
2026-05-03 10:31:14 -0300

    fix: corrige rota de listagem de hábitos para incluir o parâmetro de ID

--- src/routes/HabitRoutes.js
+++ src/routes/HabitRoutes.js
@@ -4,7 +4,7 @@ import LoginRequired from '../middlewares/LoginRequired.js';
 
 const HabitRoutes = Router();
 
-HabitRoutes.get('/habits', LoginRequired, HabitController.index);
+HabitRoutes.get('/habits/:id', LoginRequired, HabitController.index);
 HabitRoutes.post('/habits', LoginRequired, HabitController.store);
 HabitRoutes.put('/habits/:id', LoginRequired, HabitController.update);
 HabitRoutes.get('/habits/:id', LoginRequired, HabitController.show);
